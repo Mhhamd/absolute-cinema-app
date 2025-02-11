16c3a75b4841bf4dc9e4c3f04769b4eeefd12d0c
Muhammed Soliman <muhammedsoliman77@gmail.com>
2025-02-11 15:18:18 +0200

    modified the base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: 'absolute-cinema-app',
+    base: '/absolute-cinema-app/',
     plugins: [react()],
 });
